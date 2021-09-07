885e1aa16a3658fc0752dc698bb9d01ad286a62b
Aaron Alef <aaron.alef@code.berlin>
2021-09-07 13:07:03 +0200

    feat(base): include lesson02

--- include/common/utils.h
+++ include/common/utils.h
@@ -4,5 +4,6 @@
 extern void delay ( unsigned long);
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
+extern int get_el ( void );
 
 #endif  /*_BOOT_H */
