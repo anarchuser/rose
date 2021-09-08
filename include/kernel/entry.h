67b9bd7350d5c12926732f63b2130b77e5effda8
Aaron Alef <aaron.alef@code.berlin>
2021-09-08 16:48:18 +0200

    feat(base): include lesson05
    NOTE: does not work yet
    Current output:
    ```
    61
    EnableRegister: ff84110c
    Kernel process started. EL 1
    , ESR: 1fe00000, address, 1434
    ```

--- include/kernel/entry.h
+++ include/kernel/entry.h
@@ -2,6 +2,7 @@
 #define _ENTRY_H
 
 #define S_FRAME_SIZE			272 		// size of all saved registers 
+#define S_X0				0		// offset of x0 register in saved stack frame
 
 #define SYNC_INVALID_EL1t		0 
 #define IRQ_INVALID_EL1t		1 
@@ -13,7 +14,7 @@
 #define FIQ_INVALID_EL1h		6 
 #define ERROR_INVALID_EL1h		7 
 
-#define SYNC_INVALID_EL0_64	   	8 
+#define SYNC_INVALID_EL0_64	    	8 
 #define IRQ_INVALID_EL0_64	    	9 
 #define FIQ_INVALID_EL0_64		10 
 #define ERROR_INVALID_EL0_64		11 
@@ -22,6 +23,9 @@
 #define IRQ_INVALID_EL0_32		13 
 #define FIQ_INVALID_EL0_32		14 
 #define ERROR_INVALID_EL0_32		15 
+
+#define SYNC_ERROR			16 
+#define SYSCALL_ERROR			17 
 
 #ifndef __ASSEMBLER__
 
