695d151a1b1d11fea5e450128e0850bb3e59be8a
Aaron Alef <aaron.alef@code.berlin>
2021-09-10 11:43:41 +0200

    feat(std): add stdbool.h providing true, false and bool

---
+++ include/common/stdbool.h
@@ -0,0 +1,12 @@
+#ifndef _C_ROSE_BOOL_H
+#define _C_ROSE_BOOL_H
+
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined
+
+typedef char bool;
+
+#endif /*_C_ROSE_BOOL_H*/
+
+/* Copyright (C) 2020 Aaron Alef */
