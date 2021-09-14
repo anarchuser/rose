c2a0c2ed61197948f73395202cc2d1e6e4e58aa1
Aaron Alef <aaron.alef@code.berlin>
2021-09-14 15:49:28 +0200

    feat(io): add functions that use the cursor

--- include/common/screen.h
+++ include/common/screen.h
@@ -2,20 +2,37 @@
 #define _ROSE_C_SCREEN_H
 
 #include "common/printf.h"
+#include "common/stdbool.h"
 
 #define HEIGHT 79
-#define WIDTH  317
+#define WIDTH  316
 
 static char screen[HEIGHT * WIDTH];
+static int cursor_x = 0;
+static int cursor_y = 0;
 
 void clear_screen ();
 
-void draw_border ();
+void add_border ();
 
 void draw_screen ();
 
 char get_pixel (int x, int y);
 
-char set_pixel (int x, int y, char c);
+char draw_pixel(int x, int y, char c);
+char put_pixel(char c);
+char set_pixel(int x, int y, char c);
+bool fill_pixel(int x, int y, char c);
+char get_cursor();
+char set_cursor(char c);
+void move_cursor_to(int x, int y);
+void move_cursor_by(int dx, int dy);
+void reset_cursor();
+void cursor_up(int n);
+void cursor_down(int n);
+void cursor_right(int n);
+void cursor_left(int n);
+
+long combine (int x, int y);
 
 #endif //_ROSE_C_SCREEN_H
--- src/common/screen.c
+++ src/common/screen.c
@@ -1,9 +1,8 @@
 #include "common/screen.h"
 
-void clear_screen () {
-    for (int i = 0; i < HEIGHT; i ++) {
-        printf ("\n");
-    }
+void clear_screen() {
+    printf("\033[2J");
+    reset_cursor();
 }
 
 void draw_border () {
@@ -23,18 +22,18 @@ void draw_border () {
                     c = ' ';
                 }
             }
-            printf ("%c", c);
-            set_pixel (x, y, c);
+            fill_pixel (x, y, c);
         }
     }
 }
 
 void draw_screen () {
+    reset_cursor();
     for (int y = 0; y < HEIGHT; y ++) {
         for (int x = 0; x < WIDTH; x ++) {
             printf ("%c", get_pixel (x, y));
         }
-        printf ("\r\n");
+        if (y < HEIGHT - 1) printf ("\r\n");
     }
 }
 
@@ -44,4 +43,51 @@ char get_pixel (int x, int y) {
 
 char set_pixel (int x, int y, char c) {
     return screen[x + WIDTH * y] = c;
+}
+
+char fill_pixel(int x, int y, char c) {
+    if (!get_pixel(x, y)) {
+        screen [x + WIDTH * y] = c;
+        return true;
+    }
+    return false;
+}
+
+char get_cursor() {
+    return get_pixel (cursor_x, cursor_y);
+}
+
+char set_cursor(char c) {
+    return set_pixel (cursor_x, cursor_y, c);
+}
+
+void move_cursor_to(int x, int y) {
+    move_cursor_by (cursor_x - x, cursor_y - y);
+}
+
+void move_cursor_by(int dx, int dy) {
+    if (dx < 0) cursor_left (-dx);
+    if (dx > 0) cursor_right (dx);
+    if (dy < 0) cursor_up (-dy);
+    if (dy > 0) cursor_down (dy);
+}
+
+void reset_cursor() {
+    printf("\033[0;0H");
+}
+void cursor_up(int n) {
+    printf("\033[%dA", n);
+}
+void cursor_down(int n) {
+    printf("\033[%dB", n);
+}
+void cursor_right(int n) {
+    printf("\033[%dC", n);
+}
+void cursor_left(int n) {
+    printf("\033[%dD", n);
+}
+
+long combine (int x, int y) {
+    return (((long) x) << 32) + y;
 }
