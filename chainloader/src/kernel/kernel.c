986772f23f0369a0971002bff7389627e28e9f7d
Max Streitberger <max.streitberger@gmail.com>
2021-09-30 14:07:28 +0200

    feat(chainload): basic receive

--- chainloader/src/kernel/kernel.c
+++ chainloader/src/kernel/kernel.c
@@ -1,11 +1,33 @@
 #include "kernel/mini_uart.h"
+#include "common/printf.h"
 
-void kernel_main(void)
-{
+void kernel_main(void) {
 	uart_init();
+	init_printf(0, putc);
 	uart_send_string("Hello, world!\r\n");
 
-	while (1) {
-		uart_send(uart_recv());
+	char* start_adr = (char *)0x40000000;
+	char* kernel_adr = start_adr;
+
+	for (int i = 0; i < 1; i++) { //4 is size of int
+		char received = uart_recv();
+		*kernel_adr = received;
+		kernel_adr++;
 	}
+
+	int img_length = *(int *)start_adr;
+	printf("%d\n\r", img_length);
+
+	kernel_adr = start_adr;
+
+	for (int i = img_length; i > 0; i--) {
+		char received = uart_recv();
+		*kernel_adr = received;
+		kernel_adr++;
+		*kernel_adr = 0;
+
+		printf("%d\r", (int)kernel_adr - (int)start_adr);
+	}
+
+	uart_send_string((char *)start_adr);
 }
