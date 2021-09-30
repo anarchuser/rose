edebaef9c8cad6b9fae8d0f83d61a82dd3e8109b
Aaron Alef <aaron.alef@code.berlin>
2021-09-30 10:02:47 +0200

    feat(boot): add lesson01 as chainloader. Doesn't do anything yet

---
+++ chainloader/include/common/utils.h
@@ -0,0 +1,8 @@
+#ifndef	_ROSE_C_BOOT_H
+#define	_ROSE_C_BOOT_H
+
+extern void delay ( unsigned long);
+extern void put32 ( unsigned long, unsigned int );
+extern unsigned int get32 ( unsigned long );
+
+#endif  /*_ROSE_C_BOOT_H */
