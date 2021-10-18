3ea0e4b52b98ba725d64e1f8ef6dc79ce34bd350
AtomToast <reg-2@t-online.de>
2021-10-18 17:55:36 +0200

    feat(mmu): check if we received a page

--- thorn/src/kernel/paging.c
+++ thorn/src/kernel/paging.c
@@ -1,7 +1,12 @@
 #include "kernel/paging.h"
+#include "common/logging.h"
 
 mm_table_t * get_page_table (unsigned short upper_attributes, unsigned short lower_attributes, bool is_block) {
     mm_table_t * page = (mm_table_t *) get_free_page ();
+    if (page == 0) {
+        LOG("Out of memory! No page was returned!");
+        while (1) {}
+    }
     for (int i = 0; i < 512; i++) {
         mm_descriptor_t * desc = page->descriptors + i * sizeof (unsigned long);
         desc->valid = 0;
