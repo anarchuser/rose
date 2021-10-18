a24a30ad7bdb51b32a48f9670dd19bfe35825895
AtomToast <reg-2@t-online.de>
2021-10-18 18:45:05 +0200

    chore: fix some warnings and remove trailing whitespace (#29)

--- thorn/src/common/status_led.c
+++ thorn/src/common/status_led.c
@@ -4,27 +4,27 @@ bool get_led(int pin) {
                                                 // Try to read ACT LED state
     int c = 0;                                   // Message size; increment while we write
     led_msg_buffer[++c] = 0;                     // Response - will be 0x80000000 for SUCCESS or 0x80000001 for FAILURE
-        
+
     led_msg_buffer[++c] = 0x00030041;            // Tag to get ACT LED state
     led_msg_buffer[++c] = 8;                     // Size of value buffer
     led_msg_buffer[++c] = 0;                     // Response & value buffer size written will be written here
-    led_msg_buffer[++c] = pin;            // 
-    led_msg_buffer[++c] = 0;                     // 
+    led_msg_buffer[++c] = pin;            //
+    led_msg_buffer[++c] = 0;                     //
     int index_led = c;
-    
+
     led_msg_buffer[++c] = 0;                     // End tag
-    
+
     led_msg_buffer[0] = (4 * ++c);               // Write message size at the beginning of the buffer
-    
+
     if (!mailbox_request (led_msg_buffer, PROPERTY_ARM_VC)) {
         printf("ERROR\r\n");
     };
     return !led_msg_buffer[index_led];
 }
 
-/** 
+/**
  * Change LED state
- * Pins: 
+ * Pins:
  *  - Power LED: 130 (0 to turn on and 1 to turn off)
  *  - ACT LED: 42 (1 to turn on and 0 to turn off)
 **/
@@ -32,22 +32,20 @@ void set_led(bool status, int pin) {
                                                 // Try to read ACT LED state
     int c = 0;                                   // Message size; increment while we write
     led_msg_buffer[++c] = 0;                     // Response - will be 0x80000000 for SUCCESS or 0x80000001 for FAILURE
-        
+
     led_msg_buffer[++c] = 0x00038041;            // Tag to get ACT LED state
     led_msg_buffer[++c] = 8;                     // Size of value buffer
     led_msg_buffer[++c] = 0;                     // Response & value buffer size written will be written here
     led_msg_buffer[++c] = pin;
     led_msg_buffer[++c] = status;
-    int index_led = c;
-    
+
     led_msg_buffer[++c] = 0;                     // End tag
-    
+
     led_msg_buffer[0] = (4 * ++c);               // Write message size at the beginning of the buffer
-    
+
     if (!mailbox_request (led_msg_buffer, PROPERTY_ARM_VC)) {
         printf("ERROR\r\n");
     };
-    return led_msg_buffer[index_led];
 }
 
 bool toggle_led (int pin) {
