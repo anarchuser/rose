74718c33e922468ed9ffd36f9be990e0ba1f1d57
Max Streitberger <max.streitberger@gmail.com>
2021-10-20 10:52:48 +0200

    fix(chainload): remove merge artifacts

--- chainloader/src/kernel/load.c
+++ chainloader/src/kernel/load.c
@@ -8,15 +8,16 @@
 //    printf ("%c%c ", first, second);
 //}
 
-void little_endian(unsigned int * value) {
+void little_endian (unsigned int * value) {
     unsigned char * byte = (unsigned char *) value;
-    if (! * byte) return;
+    if (! *byte)
+        return;
 
     unsigned char temp = byte[0];
-    byte[0] = byte[3];
-    byte[3] = temp;
-    
-    temp = byte[1];
+    byte[0]            = byte[3];
+    byte[3]            = temp;
+
+    temp    = byte[1];
     byte[1] = byte[2];
     byte[2] = temp;
 }
@@ -24,41 +25,29 @@ void little_endian(unsigned int * value) {
 void receive_kernel (void) {
     char * const kernel_load_address = (char *) LOAD_ADDRESS;
     char * const kernel_size_address = kernel_load_address - sizeof (int);
-<<<<<<< HEAD
-    
+
     uart_send_string ("Ready to receive ");
-    for (int i = sizeof (int); i > 0;) { // 4 is size of int
-        kernel_size_address[--i] = uart_recv ();
-    }
-    int length = * (int *) kernel_size_address;
-
-    uart_send_string ("- loading kernel... ");
-    
-=======
-
-    uart_send_string ("Ready to receive\r\n");
     for (int i = sizeof (int); i > 0;) {// 4 is size of int
         kernel_size_address[--i] = uart_recv ();
     }
     int length = *(int *) kernel_size_address;
 
->>>>>>> main
+    uart_send_string ("- loading kernel... ");
+
     /** IMPORTANT **/
     /// FROM HERE ONWARDS, CHAIN LOADER OVERWRITES ITSELF. NO FURTHER NORMAL FUNCTION CALLS POSSIBLE! ///
     for (int i = 0; i < length; i++) {
         UART_RECEIVE (kernel_load_address[i]);
     }
 
-<<<<<<< HEAD
-    while (!((* (unsigned int *) AUX_MU_LSR_REG) & 0x20));
-    * (unsigned int *) AUX_MU_IO_REG = ':';
-    * (unsigned int *) AUX_MU_IO_REG = ')';
-    * (unsigned int *) AUX_MU_IO_REG = '\r';
-    * (unsigned int *) AUX_MU_IO_REG = '\n';
+    while (! ((*(unsigned int *) AUX_MU_LSR_REG) & 0x20))
+        ;
+    *(unsigned int *) AUX_MU_IO_REG = ':';
+    *(unsigned int *) AUX_MU_IO_REG = ')';
+    *(unsigned int *) AUX_MU_IO_REG = '\r';
+    *(unsigned int *) AUX_MU_IO_REG = '\n';
 
-    
-=======
->>>>>>> main
+
     // Restore system registers
     asm("mov x0, x20");
     asm("mov x1, x21");
