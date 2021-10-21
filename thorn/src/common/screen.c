dd73b5158f857caa815c776e3aef2d200307c6c9
Max Streitberger <max.streitberger@gmail.com>
2021-10-21 10:51:48 +0200

    fix(bitmap): remove get_fb() from pixel location

--- thorn/src/common/screen.c
+++ thorn/src/common/screen.c
@@ -1,10 +1,9 @@
 #include "common/screen.h"
-#define FB_PITCH GPU_COLOUR_DEPTH * GPU_SCREEN_WIDTH
-//static color_t * fb = get_fb();
-void printc(char c){
-};
+#define FB_PITCH GPU_COLOUR_DEPTH *GPU_SCREEN_WIDTH
+// static color_t * fb = get_fb();
+void printc(char c){};
 
-void drawpx (unsigned int x, unsigned int y, color_t color){
-    unsigned long int location = get_fb() + y * FB_PITCH + x * GPU_COLOUR_DEPTH;
-    get_fb()[location] = color;
+void drawpx(unsigned int x, unsigned int y, color_t color) {
+  unsigned long int location = y * FB_PITCH + x * GPU_COLOUR_DEPTH;
+  get_fb()[location] = color;
 };
