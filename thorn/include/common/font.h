356c0f78e4c11fc3f26538583080e06fac090aeb
Max Streitberger <max.streitberger@gmail.com>
2021-10-21 13:58:46 +0200

    feat(bitmap): working on printing char

--- thorn/include/common/font.h
+++ thorn/include/common/font.h
@@ -1,8 +1,11 @@
 #ifndef _ROSE_C_FONT_H
 #define _ROSE_C_FONT_H
 
-#define FONT_SIZE 8
+#define FONT_SIZE 8 * 5
+
+#include "common/screen.h"
 
 const unsigned long long int * font (int c);
+void printc_location(char c, unsigned int x, unsigned int y);
 
 #endif
