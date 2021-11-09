b7ce1c1a71c07bf90f326b88e6dbf696d223efd8
Aaron Alef <aaron.alef@code.berlin>
2021-11-09 11:46:28 +0100

    feat(font): replace font with extended ascii font

--- thorn/include/common/font.h
+++ thorn/include/common/font.h
@@ -2,7 +2,7 @@
 #define _ROSE_C_FONT_H
 
 #define FONT_SIZE       8
-#define FONT_FACTOR     2
+#define FONT_FACTOR     4
 #define FONT_REAL_WIDTH (FONT_SIZE * FONT_FACTOR)
 
 // Spacing between lines in pixels
@@ -20,6 +20,7 @@
 
 #include "common/gpu.h"
 #include "common/screen.h"
+#include "kernel/mini_uart.h"
 #include "kernel/mm.h"
 
 static point_t cursor = {0, 0};
@@ -29,10 +30,11 @@ static volatile color_t font_normal_fg = {0xC0, 0xC0, 0xC0, 0x00};
 static volatile color_t font_error_fg  = {0x00, 0x00, 0xC0, 0x00};
 static volatile color_t font_fg;
 
-const unsigned long long int * font (int c);
+unsigned char const * font (unsigned char c);
 
 void printc (char c);
 void printc_location (point_t point, char c);
+void prints_location (point_t point, char const * s);
 
 void putc_screen (void * p, char c);
 
