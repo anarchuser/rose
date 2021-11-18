ae282cefed28f31895fece2196071e2645ec7cfb
Markus Merklinger <43041831+code-mm@users.noreply.github.com>
2021-11-18 14:32:37 +0100

    Refactor irq (#61)
    * Changes definition names to actual register names
    
    * Groups definitions and adds comments
    
    * Renames base defintions
    
    * Makes handle_irq function consider first 10 Bit of GICC_IAR register
    
    * Makes handle_irq function use AUX interrupt on videocore for mini_uart interrupts
    
    * Removes invalid interrupt activation
    
    * Removes unused definitions
    
    * Adds comment for interrupt definitions
    
    * Refactor interrupt handler to use switch-case
    
    * Formats source in header file
    
    * Formats source with clang-format
    
    * Changes from hey to binary format for determining bits in GICC_IAR register
    
    * Adds conditional for uart interrupt assignment and enabling

--- thorn/include/kernel/peripherals/irq.h
+++ thorn/include/kernel/peripherals/irq.h
@@ -3,35 +3,28 @@
 
 #include "kernel/peripherals/base.h"
 
-#define GIC_BASE       0xFF840000
-#define GICD_DIST_BASE (GIC_BASE + 0x00001000)
-#define GICC_CPU_BASE  (GIC_BASE + 0x00002000)
+// GIC (Generic Interupt Controller)
+#define GIC_BASE 0xFF840000
 
-#define GICD_ENABLE_IRQ_BASE (GICD_DIST_BASE + 0x00000100)
+// GICC registers (GIC CPU register)
+#define GICC_BASE (GIC_BASE + 0x00002000)
+#define GICC_IAR  (GICC_BASE + 0x0000000C)
+#define GICC_EOIR (GICC_BASE + 0x00000010)
 
-#define GICC_IAR  (GICC_CPU_BASE + 0x0000000C)
-#define GICC_EOIR (GICC_CPU_BASE + 0x00000010)
+// GICD registers (GIC Distributor registers)
+#define GICD_BASE       (GIC_BASE + 0x00001000)
+#define GICD_ISENABLERN (GICD_BASE + 0x00000100)
+#define GICD_ITARGETSRN (GICD_BASE + 0x00000800)
 
-#define GIC_IRQ_TARGET_BASE (GICD_DIST_BASE + 0x00000800)
-
-//VC (=VideoCore) starts at 96
+/*
+* GIC interrupts
+*
+* IRQ 96-63 VideoCore interrupts
+*/
 #define SYSTEM_TIMER_IRQ_0 (0x60)//96
 #define SYSTEM_TIMER_IRQ_1 (0x61)//97
 #define SYSTEM_TIMER_IRQ_2 (0x62)//98
 #define SYSTEM_TIMER_IRQ_3 (0x63)//99
-
-#define IRQ_BASIC_PENDING  (PBASE + 0x0000B200)
-#define IRQ_PENDING_1      (PBASE + 0x0000B204)
-#define IRQ_PENDING_2      (PBASE + 0x0000B208)
-#define FIQ_CONTROL        (PBASE + 0x0000B20C)
-#define ENABLE_IRQS_1      (PBASE + 0x0000B210)
-#define ENABLE_IRQS_2      (PBASE + 0x0000B214)
-#define ENABLE_BASIC_IRQS  (PBASE + 0x0000B218)
-#define DISABLE_IRQS_1     (PBASE + 0x0000B21C)
-#define DISABLE_IRQS_2     (PBASE + 0x0000B220)
-#define DISABLE_BASIC_IRQS (PBASE + 0x0000B224)
-
-#define MINI_UART_IRQ  (1 << 0)
-#define ENABLE_AUX_INT (1 << 29)
+#define AUX_CUMULATIVE     (0x7D)// 125 Cumulates UART1, SPI1, and SPI2
 
 #endif /*_ROSE_K_P_IRQ_H */
--- thorn/src/kernel/irq.c
+++ thorn/src/kernel/irq.c
@@ -7,14 +7,14 @@ void enable_interrupt (unsigned int irq) {
     printf ("Interrupt pointer %p\r\n", irq);
     unsigned int n              = irq / 32;
     unsigned int offset         = irq % 32;
-    unsigned int enableRegister = GICD_ENABLE_IRQ_BASE + (4 * n);
+    unsigned int enableRegister = GICD_ISENABLERN + (4 * n);
     printf ("EnableRegister: %x\r\n", enableRegister);
     put32 (enableRegister, 1 << offset);
 }
 
 void assign_target (unsigned int irq, unsigned int cpu) {
     unsigned int n              = irq / 4;
-    unsigned int targetRegister = GIC_IRQ_TARGET_BASE + (4 * n);
+    unsigned int targetRegister = GICD_ITARGETSRN + (4 * n);
     // Currently we only enter the target CPU 0
     put32 (targetRegister, get32 (targetRegister) | (1 << 8));
 }
@@ -25,28 +25,32 @@ void show_invalid_entry_message (int type, ptr_t esr, ptr_t address) {
 
 void enable_interrupt_controller () {
     assign_target (SYSTEM_TIMER_IRQ_1, 0);
-    put32 (ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1
 #ifdef _ROSE_K_MINI_UART_H
-                                  | ENABLE_AUX_INT
+    assign_target (AUX_CUMULATIVE, 0);
 #endif
-    );
+
     enable_interrupt (SYSTEM_TIMER_IRQ_1);
+#ifdef _ROSE_K_MINI_UART_H
+    enable_interrupt (AUX_CUMULATIVE);
+#endif
 }
 
 void handle_irq (void) {
-    unsigned int irq_ack_reg = get32 (GICC_IAR);
-    unsigned int irq1        = irq_ack_reg & 0x2FF;
-    if (irq1 & SYSTEM_TIMER_IRQ_1) {
-        put32 (GICC_EOIR, irq_ack_reg);
-        handle_timer_irq ();
-    }
+    unsigned int irq = get32 (GICC_IAR) & 0b1111111111;// Use first 10 Bit of register
 
+    switch (irq) {
+        case (SYSTEM_TIMER_IRQ_1):
+            put32 (GICC_EOIR, irq);
+            handle_timer_irq ();
+            break;
 #ifdef _ROSE_K_MINI_UART_H
-    unsigned int irq2 = get32 (AUX_IRQ_REG);
-    if (irq2 & MINI_UART_IRQ) {
-        put32 (GICC_EOIR, irq_ack_reg);
-        handle_mini_uart_irq ();
-    }
-
+        case (AUX_CUMULATIVE):
+            put32 (GICC_EOIR, irq);
+            handle_mini_uart_irq ();
+            break;
 #endif
+        default:
+            printf ("Unhandled interrupt received: %x\r\n", irq);
+            put32 (GICC_EOIR, irq);
+    }
 }
