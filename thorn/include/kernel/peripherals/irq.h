1c061697619c219d01879cf98d7c42e97e525416
Markus Merklinger <markus.merklinger@code.berlin>
2021-11-18 13:35:11 +0100

    Formats source in header file

--- thorn/include/kernel/peripherals/irq.h
+++ thorn/include/kernel/peripherals/irq.h
@@ -4,15 +4,15 @@
 #include "kernel/peripherals/base.h"
 
 // GIC (Generic Interupt Controller)
-#define GIC_BASE       0xFF840000
+#define GIC_BASE    0xFF840000
 
 // GICC registers (GIC CPU register)
-#define GICC_BASE  (GIC_BASE + 0x00002000)
-#define GICC_IAR  (GICC_BASE + 0x0000000C)
-#define GICC_EOIR (GICC_BASE + 0x00000010)
+#define GICC_BASE   (GIC_BASE + 0x00002000)
+#define GICC_IAR    (GICC_BASE + 0x0000000C)
+#define GICC_EOIR   (GICC_BASE + 0x00000010)
 
 // GICD registers (GIC Distributor registers)
-#define GICD_BASE (GIC_BASE + 0x00001000)
+#define GICD_BASE       (GIC_BASE + 0x00001000)
 #define GICD_ISENABLERN (GICD_BASE + 0x00000100)
 #define GICD_ITARGETSRN (GICD_BASE + 0x00000800)
 
@@ -25,6 +25,6 @@
 #define SYSTEM_TIMER_IRQ_1 (0x61)//97
 #define SYSTEM_TIMER_IRQ_2 (0x62)//98
 #define SYSTEM_TIMER_IRQ_3 (0x63)//99
-#define AUX_CUMULATIVE (0x7D) // 125 Cumulates UART1, SPI1, and SPI2
+#define AUX_CUMULATIVE     (0x7D) // 125 Cumulates UART1, SPI1, and SPI2
 
 #endif /*_ROSE_K_P_IRQ_H */
