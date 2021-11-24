37deadc260ab9d2f132a23afcc1a77e062d67ad1
Markus Merklinger <markus.merklinger@code.berlin>
2021-11-24 11:23:09 +0100

    Adds ethernet includes with memory addresses

---
+++ thorn/include/kernel/ethernet.h
@@ -0,0 +1,9 @@
+#ifndef _ROSE_K_ETHERNET_H
+#define _ROSE_K_ETHERNET_H
+
+#include "common/printf.h"
+#include "common/stddef.h"
+#include "common/utils.h"
+#include "kernel/peripherals/ethernet.h"
+
+#endif /*_ROSE_K_ETHERNET_H */
